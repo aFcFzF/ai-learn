a8c60a31a92bd6618716e036b24407c4928a0e6f
afcfzf <9301462@qq.com>
2025-03-24 20:58:03 +0800

    update

--- ml/visual/captcha/knn.py
+++ ml/visual/captcha/knn.py
@@ -3,3 +3,24 @@ import os
 from PIL import Image
 import numpy as np
 import shutil
+
+x = []
+y = []
+
+for label in os.listdir('train'):
+    for file in os.listdir('train/{}'.format(label)):
+        im = Image.open('train/{}/{}'.format(label, file))
+        im_gray = im.convert('L')
+        pix =np.array(im_gray)
+        pix = (pix > 150) * 1
+        pix = pix.ravel()
+        x.append(list(pix))
+        y.append(int(label))
+        train_x = np.array(x)
+        train_y = np.array(y)
+        model = neighbors.KNeighborsClassifier(n_neighbors=32)
+        model.fit(train_x, train_y)
+
+        
+
+
