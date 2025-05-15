4ccb8ff17d86dc0ef8ee24fddad0f6048db372f9
afcfzf <9301462@qq.com>
2025-05-15 19:41:09 +0800

    update

--- share/yolo-train/train.py
+++ share/yolo-train/train.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 model = YOLO(model="yolo11n.pt", task="detect")
-results = model.train(data="train.yaml", epochs=2000, batch=100, patience=30)
 
-print(results)
+results = model.train(data="train.yaml", project="./", epochs=1, batch=100, patience=3, name="runs/train")
+
+model.export(format="onnx")
+
