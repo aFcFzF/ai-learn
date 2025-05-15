734f83e3865ba608d87e118252edd58c8992d405
afcfzf <9301462@qq.com>
2025-05-16 00:52:03 +0800

    chore

--- share/proj/mole/play/main.py
+++ share/proj/mole/play/main.py
@@ -1,6 +1,10 @@
 import pyautogui
+from ultralytics import YOLO
 
 def main():
+    model = YOLO(model="yolo11s.pt", task="detect")
+    results = model.train(data="train.yaml", project="./", epochs=200, batch=100, patience=50, name="runs/train")
+
     screenWidth, screenHeight = pyautogui.size()
     print(screenWidth, screenHeight)
     pyautogui.moveTo(250, 268)
