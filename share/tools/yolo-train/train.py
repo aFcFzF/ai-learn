978cbf4e7e12a3659f13a1387156521c67ce62a3
markjia <markjia@tencent.com>
2025-05-16 00:48:08 +0800

    feat: update

--- share/tools/yolo-train/train.py
+++ share/tools/yolo-train/train.py
@@ -1,6 +1,13 @@
 from ultralytics import YOLO
-model = YOLO(model="yolo11s.pt", task="detect")
+model = YOLO(model="yolo11s.pt")
 
-results = model.train(data="train.yaml", project="./", epochs=200, batch=100, patience=50, name="runs/train")
+results = model.train(
+  data="/data/workspace/ai-learn/share/proj/mole/train/datasets/best/train.yaml",
+  project="/data/workspace/ai-learn/share/proj/mole/train/datasets/best",
+  epochs=10,
+  batch=50,
+  patience=200,
+  name="runs/train"
+)
 
 model.export(format="onnx")
